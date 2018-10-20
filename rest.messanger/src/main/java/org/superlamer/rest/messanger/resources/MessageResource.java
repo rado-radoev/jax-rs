9f24701a777fe97674c895e059d1a34fbe80556d
Radoslav Radoev <rradoev@gmail.com>
2018-10-20 00:12:44 -0700

    implemented post, put, delete methods

--- rest.messanger/src/main/java/org/superlamer/rest/messanger/resources/MessageResource.java
+++ rest.messanger/src/main/java/org/superlamer/rest/messanger/resources/MessageResource.java
@@ -1,8 +1,12 @@
 package org.superlamer.rest.messanger.resources;
 
-import java.util.List;  
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,21 +16,41 @@ import org.superlamer.rest.messanger.model.Message;
 import org.superlamer.rest.messanger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") Long messageId) {
 		return messageService.getMessage(messageId);
 	}
+	
+	@POST
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(Message message, @PathParam("messageId") long id) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
+	}
+	
 
 }
