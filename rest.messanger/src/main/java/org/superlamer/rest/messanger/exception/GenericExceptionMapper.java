a05819d3a0d633bf0bf2c89548ad151fa5fad77c
Radoslav Radoev <rradoev@gmail.com>
2018-10-22 01:10:34 -0700

    added generic exception handler

---
+++ rest.messanger/src/main/java/org/superlamer/rest/messanger/exception/GenericExceptionMapper.java
@@ -0,0 +1,22 @@
+package org.superlamer.rest.messanger.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.superlamer.rest.messanger.model.Error;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable>{
+	
+	@Override
+	public Response toResponse(Throwable exception) {
+		Error errorMessage = new Error(exception.getMessage(), 500,"https://stackoverflow.com/");
+		
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+				.entity(errorMessage)
+				.build();
+	}
+
+}
